7a479ea7f2b10f2fe8936afc0882c5e285532ad4
Dan Fandrich <dan@coneharvesters.com>
2012-09-02 22:09:59 +0200

    Include sys/types.h in unixtime.h to get time_t
    Patch comes from FreeBSD ports

--- unixtime.h
+++ unixtime.h
@@ -25,6 +25,8 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
+#include <sys/types.h>
+
 #define EXIF_DATE_FORMAT "%d:%d:%d %d:%d:%d"
 #define GPX_DATE_FORMAT "%d-%d-%dT%d:%d:%dZ"
 
